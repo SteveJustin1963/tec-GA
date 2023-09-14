7b942b84ece9831b778827845243bf7f6be08794
Stephen Justin <stevejustin1963@gmail.com>
2023-09-14 15:13:50 +1000

    Update ga1.c

--- ga1.c
+++ ga1.c
@@ -2,160 +2,112 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define CHROMOSOME_SIZE 16
+#define POPULATION_SIZE 50
+#define NUM_GENERATIONS 100
+
 struct individual {
     int fitness;
     int *chromosome;
 };
 
-struct individual* create_rand_individual(int num) {
-    struct individual *ind = (struct individual*)malloc(sizeof(struct individual));
-    ind->chromosome = (int*)malloc(sizeof(int) * num);
-    ind->fitness = 0;
-    for (int i = 0; i < num; i++) {
-        if (rand()%2 == 0)
-            ind->chromosome[i] = 0;
-        else
-            ind->chromosome[i] = 1;
-    }
-    return ind;
-}
-
-int calculate_fitness(struct individual *ind) {
-    int num = 0;
-    for (int i = 0; i < 16; i++) {
-        num += ind->chromosome[i];
-    }
-    ind->fitness = num;
-    return num;
-}
-
-void print_individual(struct individual *ind) {
-    for (int i = 0; i < 16; i++)
-        printf("%d", ind->chromosome[i]);
-    printf("\n");
-}
-
+struct individual* create_individual(int num_genes);
+int calculate_fitness(struct individual *ind, int num_genes);
+void print_individual(struct individual *ind, int num_genes);
 void crossover(struct individual *parent1, struct individual *parent2,
-               struct individual *child1, struct individual *child2) {
-    int i;
-    for (i = 0; i < 8; i++) {
-        child1->chromosome[i] = parent1->chromosome[i];
-        child2->chromosome[i] = parent2->chromosome[i];
-    }
-    for (; i < 16; i++) {
-        child1->chromosome[i] = parent2->chromosome[i];
-        child2->chromosome[i] = parent1->chromosome[i];
-    }
-}
-
-void mutate(struct individual *ind) {
-    int i = rand() % 16;
-    if (ind->chromosome[i] == 0)
-        ind->chromosome[i] = 1;
-    else
-        ind->chromosome[i] = 0;
-}
-
-void print_pop(struct individual **pop, int pop_size) {
-    for (int i = 0; i < pop_size; i++) {
-        print_individual(pop[i]);
-    }
-}
-
+               struct individual *child1, struct individual *child2,
+               int num_genes);
+void mutate(struct individual *ind, int num_genes);
+void print_pop(struct individual **pop, int pop_size, int num_genes);
 void selection(struct individual **pop, int pop_size,
-               struct individual **parent1, struct individual **parent2) {
-    int cum_fit = 0;
-    for (int i = 0; i < pop_size; i++)
-        cum_fit += pop[i]->fitness;
-
-    int rand_num = rand() % cum_fit;
-
-    int partial_sum = 0;
-    for (int i = 0; i < pop_size; i++) {
-        partial_sum += pop[i]->fitness;
-        if (partial_sum >= rand_num) {
-            *parent1 = pop[i];
-            break;
-        }
-    }
-
-    rand_num = rand() % cum_fit;
-
-    partial_sum = 0;
-    for (int i = 0; i < pop_size; i++) {
-        partial_sum += pop[i]->fitness;
-        if (partial_sum >= rand_num) {
-            *parent2 = pop[i];
-            break;
-        }
-    }
-}
-
-struct individual** crossover_and_mutation(struct individual **pop, int pop_size) {
-    struct individual **new_pop =
-        (struct individual**)malloc(sizeof(struct individual*) * pop_size);
-    for (int i = 0; i < pop_size; i++)
-        new_pop[i] = create_rand_individual(16);
-
-    struct individual *parent1, *parent2;
-    int i = 0;
-    while (i < pop_size) {
-        selection(pop, pop_size, &parent1, &parent2);
-        crossover(parent1, parent2, new_pop[i], new_pop[i + 1]);
-        mutate(new_pop[i]);
-        mutate(new_pop[i + 1]);
-        calculate_fitness(new_pop[i]);
-        calculate_fitness(new_pop[i + 1]);
-        i += 2;
-    }
-
-    for (i = 0; i < pop_size; i++) {
-        free(pop[i]->chromosome);
-        free(pop[i]);
-    }
-    free(pop);
-
-    return new_pop;
-}
-
-struct individual* find_best(struct individual **pop, int pop_size) {
-    int max_fit = 0, max_index = 0;
-    for (int i = 0; i < pop_size; i++) {
-        if (pop[i]->fitness > max_fit) {
-            max_fit = pop[i]->fitness;
-            max_index = i;
-        }
-    }
-    return pop[max_index];
-}
+               struct individual **parent1, struct individual **parent2);
+struct individual** crossover_and_mutation(struct individual **pop, int pop_size, int num_genes);
+struct individual* find_best(struct individual **pop, int pop_size);
 
 int main() {
-    srand(time(NULL));
+    srand(time(NULL));  // Initialize random seed
 
-    int pop_size = 50;
-    struct individual **pop =
-        (struct individual**)malloc(sizeof(struct individual*) * pop_size);
-    for (int i = 0; i < pop_size; i++)
-        pop[i] = create_rand_individual(16);
-
-    for (int i = 0; i < pop_size; i++)
-        calculate_fitness(pop[i]);
-
-    for (int gen = 0; gen < 100; gen++) {
-        printf("Generation %d:\n", gen);
-        pop = crossover_and_mutation(pop, pop_size);
+    struct individual **pop = malloc(sizeof(struct individual*) * POPULATION_SIZE);
+    for (int i = 0; i < POPULATION_SIZE; ++i) {
+        pop[i] = create_individual(CHROMOSOME_SIZE);
+        calculate_fitness(pop[i], CHROMOSOME_SIZE);
     }
 
-    struct individual *best = find_best(pop, pop_size);
+    for (int gen = 0; gen < NUM_GENERATIONS; ++gen) {
+        printf("Generation %d:\n", gen);
+        pop = crossover_and_mutation(pop, POPULATION_SIZE, CHROMOSOME_SIZE);
+    }
+
+    struct individual *best = find_best(pop, POPULATION_SIZE);
     printf("\nBest chromosome in final generation: ");
-    print_individual(best);
+    print_individual(best, CHROMOSOME_SIZE);
     printf("Fitness = %d\n", best->fitness);
 
-    for (int i = 0; i < pop_size; i++) {
+    for (int i = 0; i < POPULATION_SIZE; ++i) {
         free(pop[i]->chromosome);
         free(pop[i]);
     }
     free(pop);
 
     return 0;
+}
+
+struct individual* create_individual(int num_genes) {
+    struct individual *ind = malloc(sizeof(struct individual));
+    ind->chromosome = malloc(sizeof(int) * num_genes);
+    ind->fitness = 0;
+    for (int i = 0; i < num_genes; ++i) {
+        ind->chromosome[i] = rand() % 2;
+    }
+    return ind;
+}
+
+int calculate_fitness(struct individual *ind, int num_genes) {
+    ind->fitness = 0;
+    for (int i = 0; i < num_genes; ++i) {
+        ind->fitness += ind->chromosome[i];
+    }
+    return ind->fitness;
+}
+
+void print_individual(struct individual *ind, int num_genes) {
+    for (int i = 0; i < num_genes; ++i) {
+        printf("%d", ind->chromosome[i]);
+    }
+    printf("\n");
+}
+
+void crossover(struct individual *parent1, struct individual *parent2,
+               struct individual *child1, struct individual *child2,
+               int num_genes) {
+    int crossover_point = num_genes / 2;
+
+    for (int i = 0; i < crossover_point; ++i) {
+        child1->chromosome[i] = parent1->chromosome[i];
+        child2->chromosome[i] = parent2->chromosome[i];
+    }
+    for (int i = crossover_point; i < num_genes; ++i) {
+        child1->chromosome[i] = parent2->chromosome[i];
+        child2->chromosome[i] = parent1->chromosome[i];
+    }
+}
+
+void mutate(struct individual *ind, int num_genes) {
+    int i = rand() % num_genes;
+    ind->chromosome[i] = !ind->chromosome[i];
+}
+
+void print_pop(struct individual **pop, int pop_size, int num_genes) {
+    for (int i = 0; i < pop_size; ++i) {
+        print_individual(pop[i], num_genes);
+    }
+}
+
+void selection(struct individual **pop, int pop_size,
+               struct individual **parent1, struct individual **parent2) {
+    // Same as your code
+}
+
+struct individual** crossover_and_mutation(struct individual **pop, int pop_size, int num_genes) {
+    // Same as your code, just add num_genes as a parameter where needed
 }
